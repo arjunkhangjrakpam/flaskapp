6a3320cb97a34ec2a83a2811e2631ab01e0537d4
arjunkhangjrakpam <yooarjun@gmail.com>
2021-01-14 19:09:35 +0530

    Intro to Cloud Azure commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!. Flask app successfully deployed."
+    return "Hello World!. Flask app successfully deployed"
 
 if __name__ == '__main__':
     app.run()
